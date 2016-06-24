fd528d0c8b8c811b922e92165030a378744a2788
zzsanduo <zzsanduo@gmail.com>
2016-06-24 19:18:44 +0800

    fix some typo and format code

--- test/copy-object.spec.js
+++ test/copy-object.spec.js
@@ -1,98 +1,98 @@
 var copy = require('../src/copy-object.js');
 
-describe('array apis',function(){
-    it('should copy an object shanlowly',function(){
-            var src = {
-                'name':'zzsanduo',
-                'info':{
-                    'hobby':'cycling'
-                },
-                'expirences':[
-                    'BUPT',
-                    'Schlumberger'
+describe('copyObject, ', function() {
+    it('can shallow copy an object', function() {
+        var src = {
+            'name': 'zzsanduo',
+            'info': {
+                'hobby': 'cycling'
+            },
+            'expirences': [
+                'BUPT',
+                'Schlumberger'
+            ]
+
+        };
+        var dest = {};
+
+        copy(src, dest);
+        src.info.hobby = 'singing';
+        src.expirences.push('future');
+
+        expect(dest.name).toBe('zzsanduo');
+        expect(dest.info.hobby).toBe('singing');
+        expect(dest.expirences.length).toBe(3);
+
+
+    });
+
+    it('can deep copy an object', function() {
+        var src = {
+            'name': 'zzsanduo',
+            'info': {
+                'hobby': 'cycling'
+            }
+        };
+        var dest = {};
+
+        copy(src, dest, true);
+        src.info.hobby = 'singing';
+
+        expect(dest.name).toBe('zzsanduo');
+        expect(dest.info.hobby).toBe('cycling');
+
+    });
+
+    it('can deep copy an object with array property', function() {
+        var src = {
+            'name': 'zzsanduo',
+            'info': {
+                'hobby': 'cycling'
+            },
+            'expirences': [
+                'BUPT',
+                'Schlumberger'
+            ]
+        };
+        var dest = {};
+
+        copy(src, dest, true);
+        src.info.hobby = 'singing';
+        src.expirences.push('future');
+
+        expect(dest.name).toBe('zzsanduo');
+        expect(dest.info.hobby).toBe('cycling');
+        expect(dest.expirences.length).toBe(2);
+        expect(src.expirences.length).toBe(3);
+    });
+
+    it('can deep copy an object with complexed property', function() {
+        var src = {
+            'name': 'zzsanduo',
+            'info': {
+                'hobby': 'cycling',
+                'family': [
+                    'baba',
+                    'mama',
+                    'husband'
                 ]
-
-            };
-            var dest = {};
-
-            copy(src,dest);
-            src.info.hobby = 'singing';
-            src.expirences.push('future');
-
-            expect(dest.name).toBe('zzsanduo');
-            expect(dest.info.hobby).toBe('singing');
-            expect(dest.expirences.length).toBe(3);
-
-
-    });
-
-    it('should copy an object deeply',function(){
-        var src = {
-                'name':'zzsanduo',
-                'info':{
-                    'hobby':'cycling'
-                }
-            };
-            var dest = {};
-
-            copy(src,dest,true);
-            src.info.hobby = 'singing';
-
-            expect(dest.name).toBe('zzsanduo');
-            expect(dest.info.hobby).toBe('cycling');   
-
-    });
-
-    it('should copy an object with array property deeply',function(){
-        var src = {
-                'name':'zzsanduo',
-                'info':{
-                    'hobby':'cycling'
-                },
-                'expirences':[
-                    'BUPT',
-                    'Schlumberger'
-                ]
-            };
-            var dest = {};
-
-            copy(src,dest,true);
-            src.info.hobby = 'singing';
-            src.expirences.push('future');
-
-            expect(dest.name).toBe('zzsanduo');
-            expect(dest.info.hobby).toBe('cycling');
-            expect(dest.expirences.length).toBe(2);
-            expect(src.expirences.length).toBe(3);
-    });
-
-     it('should copy an object with complexed property deeply',function(){
-        var src = {
-                'name':'zzsanduo',
-                'info':{
-                    'hobby':'cycling',
-                    'family':[
-                        'baba',
-                        'mama',
-                        'husband'
-                    ]
-                },
-                'expirences':[
-                    ['BUPT','BUPT'],
-                    {
-                        'Schlumberger':
-                        {'start':'2014',
-                        'end':'now'}
-                        
+            },
+            'expirences': [
+                ['BUPT', 'BUPT'], {
+                    'Schlumberger': {
+                        'start': '2014',
+                        'end': 'now'
                     }
-                ]
-            };
-            var dest = {};
 
-            copy(src,dest,true);
-            src.expirences[1].Schlumberger.start = '2014.2';
+                }
+            ]
+        };
+        var dest = {};
 
-            expect(dest.expirences[1].Schlumberger.start).toBe('2014');
+        copy(src, dest, true);
+        src.expirences[1].Schlumberger.start = '2014.2';
+
+        expect(dest.expirences[1].Schlumberger.start).toBe('2014');
     });
 
 
